d35a680b8581645f040cb70c7cfb1afdc1f9d2cc
Ranielle Fernandes <157660002+raniellefernandes@users.noreply.github.com>
2024-05-03 03:32:08 +0000

    criado sessão para contato e mudado estilo formulário

--- script.js
+++ script.js
@@ -6,14 +6,16 @@ document.getElementById('form-agendamento').addEventListener('submit', function(
   const celular = formData.get('celular');
   const servico = formData.get('servico');
   const outroServico = formData.get('outro_servico');
-  const data = formData.get('data');
+  const data = new Date(formData.get('data'));
   const horario = formData.get('horario');
+
+  const dataFormatada = data.toLocaleDateString('pt-BR');
 
   const mensagem = 'Olá, meu nome é ' + nome + ', gostaria de fazer um agendamento! \n' +
     'Serviço(s): \n- ' + servico + (outroServico ? ' \n- ' + outroServico : '') + '\n' +
-    'Data: ' + data + '\n' +
+    'Data: ' + dataFormatada + '\n' +
     'Horário: ' + horario + '\n' +
     'Meu contato: ' + celular;
 
-  window.open('https://api.whatsapp.com/send?phone=82991487903&text=' + encodeURIComponent(mensagem));
+  window.open('https://api.whatsapp.com/send?phone=119324543253&text=' + encodeURIComponent(mensagem));
 });
